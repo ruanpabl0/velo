1d7910c35b896ac308b9224443aa33c55ad06c01
ruanpabl0 <ruanpabloalmeidagomes@hotmail.com>
2026-04-27 20:06:13 -0300

    Update project aulas de CICD e TestOps

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-sprint.vercel.app',
+    // baseURL: process.env.BASE_URL || 'http://localhost:5173',
+    baseURL: 'https://velo-ruanpabl0.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
