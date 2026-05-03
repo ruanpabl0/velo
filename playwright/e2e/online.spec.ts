64ee63d0a13011731ee8da568a3c379a1a56ea89
ruanpabl0 <ruanpabloalmeidagomes@hotmail.com>
2026-05-03 11:54:40 -0300

    Validação Github Actions 01

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '../support/fixtures'
 test('webapp deve estar online', async ({ page }) => {
   await page.goto('/')
 
-  await expect(page).toHaveTitle(/Velô by Papito/)
+  await expect(page).toHaveTitle(/Velô by Papito/);
 })
